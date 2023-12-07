f7836cfcac4e0e993f1237131639eade54a89b34
Torremolinos <adrian_2054@hotmail.com>
2023-12-07 02:22:45 +0100

    problemas

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
+import { nieve } from "./copos.js";
+nieve();
 import { Product } from "./product.js";
 import { ProductManager } from "./productManager.js";
-import { nieve } from "./copos.js";
 
-nieve();
 const productManager = new ProductManager();
 const listProducts = [
   new Product(1, "Trenes", 50, 10.5),
@@ -20,65 +20,74 @@ const listProducts = [
   new Product(13, "Marioneta", 50, 7.55),
   new Product(14, "ullaHop", 50, 6.55),
 ];
-// listProducts.forEach((product) => inventory.addProduct(product));
-// storeProducts(listProducts);
-// Evento del formulario para agregar un nuevo producto.
+listProducts.forEach((product) => productManager.addProduct(product));
+updateInventoryTable();
 
-document
-  .getElementById("product-form-events")
-  .addEventListener("submit", function (event) {
-    //cuando se envie el formulario
-    event.preventDefault(); //evita que se recargue la pagina
+// Event listener for the form to add a new product
+document.getElementById("product-form-events").addEventListener("submit", function (event) {
+    event.preventDefault();
 
-    //Obtenemos los valores del formulario
-    const productName = document.getElementById("product-name").value; //obtenemos el valor del input
-    const productQuantity = parseInt(
-      document.getElementById("product-quantity").value
-    ); //obtenemos el valor del input
-    const productPrice = parseFloat(
-      document.getElementById("product-price").value
-    ); //obtenemos el valor del input
-    // Crear una instancia de Producto con los valores del formulario.
-    //Date.now() devuelve el numero de milisegundos transcurridos desde el 1 de enero de 1970 hasta la fecha actual.
+    const productName = document.getElementById("product-name").value;
+    const productQuantity = parseInt(document.getElementById("product-quantity").value);
+    const productPrice = parseFloat(document.getElementById("product-price").value);
+
+    if (!productName || isNaN(productQuantity) || isNaN(productPrice)) {
+        alert("Please enter valid data.");
+        return;
+    }
+
     const newProduct = new Product(
-      Date.now(),
-      productName,
-      productQuantity,
-      productPrice
-    ); //creamos una instancia de producto con los valores del formulario
-    //mostrar por consola el producto
-    console.log(newProduct);
+        Date.now(),
+        productName,
+        productQuantity,
+        productPrice
+    );
 
-    //Agregar el nuevo producto al administrador de productos.
     productManager.addProduct(newProduct);
-    //el this hace referencia al formulario actual.
-    this.reset(); //limpiamos el formulario
-    //Actualizamos la tabla de inventario
+    this.reset();
     updateInventoryTable();
-  });
-// Función para actualizar la tabla de inventario
+});
+
+// Function to update the inventory table
 function updateInventoryTable() {
-  const tableBody = document.getElementById("body-table");
-  tableBody.innerHTML = "";
+    const tableBody = document.getElementById("body-table");
+    tableBody.innerHTML = "";
 
-  // Obtener la lista de productos del administrador
-  const products = productManager.listProducts();
+    const products = productManager.listProducts();
 
-  // Iterar sobre la lista de productos y agregar filas a la tabla
-  products.forEach((product) => {
-    const row = document.createElement("tr");
-    row.innerHTML = `
+    products.forEach((product) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
             <td>${product.nombre}</td>
             <td>${product.cantidad}</td>
             <td>${product.precio}</td>
             <td>
-                <button id="edit-button">Editar</button>
-                <button id="delete-button">Eliminar</button>
+                <button class="edit-button" data-id="${product.id}">Editar</button>
+                <button class="delete-button" data-id="${product.id}">Eliminar</button>
             </td>
         `;
-    tableBody.appendChild(row);
-  });
+        tableBody.appendChild(row);
+    });
 }
 
-// Llamar a la función inicial para mostrar la tabla del inventario
-updateInventoryTable();
+// Event delegation for button clicks (edit and delete)
+document.getElementById("body-table").addEventListener("click", function (event) {
+    const target = event.target;
+
+    if (target.classList.contains("edit-button")) {
+        // Handle edit button click
+        const productId = parseInt(target.dataset.id);
+        const editedProduct = prompt("Enter updated product information (name, quantity, price):");
+        if (editedProduct) {
+            const [name, quantity, price] = editedProduct.split(',').map(value => value.trim());
+            const updatedProduct = new Product(productId, name, parseInt(quantity), parseFloat(price));
+            productManager.updateProductById(productId, updatedProduct);
+            updateInventoryTable();
+        }
+    } else if (target.classList.contains("delete-button")) {
+        // Handle delete button click
+        const productId = parseInt(target.dataset.id);
+        productManager.deleteProductById(productId);
+        updateInventoryTable();
+    }
+});
